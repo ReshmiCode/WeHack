a15d00c226dec6c3bc7304d90161e707ab92d9c6
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-10-11 14:20:48 -0500

    log in button

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -2,6 +2,7 @@ import logo from "../logo.png";
 import React from "react";
 import NavBar from "../components/NavBar";
 import { Layout } from "antd";
+import { NavLink } from "react-router-dom";
 const { Content } = Layout;
 
 const Lorem =
@@ -19,13 +20,25 @@ const LandingPage = () => {
   return (
     <Layout>
       <NavBar />
-      <Content className="mainContent" style={{ justifyContent:"center" }}>
-        <img src={logo}></img>
-        <div>
-          <p>
-            {" "}
+      <Content className="mainContent" style={{ justifyContent: "center" }}>
+        <img width="100%" src={logo}></img>
+        <div style={{ textAlign: "center", padding: "30px 30px 0px 30px" }}>
+          <h3 style={{ marginBottom: "35px" }}>
+            It can be hard for women to be entrepreneurs and small business
+            owners. We wanted to create an application which would encourage,
+            guide, and create a community of women entrepreneurs. We wanted to
+            connect women to the right resources they would need. Startups in
+            robotics may need funding or parts but the people in charge may not
+            know where to get these or need connections. Startups for mobile
+            applications may not need capital, just a starting user base. We
+            wanted to solve all these challenges with one solution: Empower.
+          </h3>
+          <NavLink exact className="btn btn-1" to="/companies">
+            Log In
+          </NavLink>
+          <p style={{ marginTop: "35px" }}>
             Made with 💖 by Medha Jonnada, Megan Tran, Reshmi Ranjith, and
-            Saloni Shivdasani{" "}
+            Saloni Shivdasani
           </p>
         </div>
       </Content>
