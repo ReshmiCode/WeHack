7a8baf60f7207bd8e64df0a91828e6ebefee79ef
SaloniSS <sxs170015@utdallas.edu>
2020-10-10 23:27:24 -0500

    frontend for add company

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,27 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout, Input, Button } from "antd";
 import NavBar from "../components/NavBar";
 const { Content } = Layout;
 const { TextArea } = Input;
 
 const Profile = () => {
+  let [enteredTitle, setEnteredTitle] = useState("");
+  let [enteredImage, setEnteredImage] = useState("");
+  let [enteredDescription, setEnteredDescription] = useState("");
+  let [enteredContent, setEnteredContent] = useState("");
+  let [enteredTags, setEnteredTags] = useState("");
+
+  function AddCompany() {
+    const company = {
+      title: enteredTitle,
+      avatar: enteredImage,
+      description: enteredDescription,
+      content: enteredContent,
+      tags: enteredTags.split(", "),
+    };
+    // db.collection("advice")
+    //   .add({
+    //     advice: enteredAdvice,
+    //   })
+    //   .then(function (docRef) {
+    //     console.log("Document written with ID: ", docRef.id);
+    //   })
+    //   .catch(function (error) {
+    //     console.error("Error adding document: ", error);
+    //   });
+    console.log(company);
+  }
+
   return (
     <Layout>
       <NavBar />
       <Content className="mainContent" style={{ textAlign: "center" }}>
         <h1>Enter Company Details</h1>
         <h3>Enter Company Title</h3>
-        <Input type="text" style={{ width: "30%" }} />
+        <Input
+          type="text"
+          style={{ width: "30%" }}
+          value={enteredTitle}
+          onChange={(event) => setEnteredTitle(event.target.value)}
+        />
         <h3>Enter Link to Image</h3>
-        <Input type="url" style={{ width: "50%" }} />
+        <Input
+          type="url"
+          style={{ width: "50%" }}
+          value={enteredImage}
+          onChange={(event) => setEnteredImage(event.target.value)}
+        />
         <h3>Enter Description</h3>
-        <TextArea rows="10" style={{ width: "50%" }} />
+        <TextArea
+          rows="10"
+          style={{ width: "50%" }}
+          value={enteredDescription}
+          onChange={(event) => setEnteredDescription(event.target.value)}
+        />
         <h3>Enter Content</h3>
-        <TextArea rows="10" style={{ width: "50%" }} />
+        <TextArea
+          rows="10"
+          style={{ width: "50%" }}
+          value={enteredContent}
+          onChange={(event) => setEnteredContent(event.target.value)}
+        />
         <h3>Enter Tags (seperated by commas)</h3>
-        <TextArea rows="2" style={{ width: "50%" }} />
+        <TextArea
+          rows="2"
+          style={{ width: "50%" }}
+          value={enteredTags}
+          onChange={(event) => setEnteredTags(event.target.value)}
+        />
         <br />
-        <Button type="primary">Submit Company</Button>
+        <Button type="primary" onClick={AddCompany}>
+          Submit Company
+        </Button>
       </Content>
     </Layout>
   );
