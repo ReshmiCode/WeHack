c34677b3b739d38ebe67be91b3f639c7dab23e1e
SaloniSS <sxs170015@utdallas.edu>
2020-10-10 22:48:39 -0500

    use library for components

--- src/screens/AddAdvice.js
+++ src/screens/AddAdvice.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import NavBar from "../components/NavBar";
-import { Layout, PageHeader } from "antd";
+import { Layout, Input, Button } from "antd";
 import db from "../components/firebase";
 const { Content } = Layout;
+const { TextArea } = Input;
 
 const AddAdvice = () => {
   let [enteredAdvice, setEnteredAdvice] = useState("");
@@ -27,19 +28,20 @@ const AddAdvice = () => {
     <Layout>
       <NavBar />
       <Content className="mainContent">
-        <h1 style={{ margin: 0, padding: 20 }}>
+        <h1 style={{ margin: 0, padding: 20, textAlign: "center" }}>
           Enter Advice for other Women Entrepreuners
         </h1>
         <div style={{ textAlign: "center" }}>
-          <p>Enter your Advice</p>
-          <textarea
-            cols="60"
+          <TextArea
+            cols="2"
             rows="10"
             value={enteredAdvice}
             onChange={(event) => setEnteredAdvice(event.target.value)}
           />
           <br />
-          <button onClick={saveAdvice}>Submit Advice</button>
+          <Button type="primary" onClick={saveAdvice}>
+            Submit Advice
+          </Button>
         </div>
       </Content>
     </Layout>
