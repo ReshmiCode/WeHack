732c9902b0bd1cf9b38840454614681da14a0e90
SaloniSS <sxs170015@utdallas.edu>
2020-10-10 23:19:02 -0500

    frontend forms

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Input, Button } from "antd";
 import NavBar from "../components/NavBar";
 const { Content } = Layout;
+const { TextArea } = Input;
 
 const Profile = () => {
   return (
     <Layout>
       <NavBar />
-      <Content className="mainContent">
-        <h2>hi</h2>
+      <Content className="mainContent" style={{ textAlign: "center" }}>
+        <h1>Enter Company Details</h1>
+        <h3>Enter Company Title</h3>
+        <Input type="text" style={{ width: "30%" }} />
+        <h3>Enter Link to Image</h3>
+        <Input type="url" style={{ width: "50%" }} />
+        <h3>Enter Description</h3>
+        <TextArea rows="10" style={{ width: "50%" }} />
+        <h3>Enter Content</h3>
+        <TextArea rows="10" style={{ width: "50%" }} />
+        <h3>Enter Tags (seperated by commas)</h3>
+        <TextArea rows="2" style={{ width: "50%" }} />
+        <br />
+        <Button type="primary">Submit Company</Button>
       </Content>
     </Layout>
   );
